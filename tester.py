89069437e48929694158996e16cb56551d02bcf8
gleb2803 <114805226+gleb2803@users.noreply.github.com>
2025-10-20 19:06:37 +0300

    Fix reference to player's head mesh

--- tester.py
+++ tester.py
@@ -2982,7 +2982,7 @@ button.TextWrapped = true
 button.MouseButton1Down:connect(function()
 	plr = game.Players./localplayer/.Character
 	meshes = {"Brick","Cylinder","Head","Sphere","Torso","Wedge"}
-	h = game.Workspace[plr].Head.Mesh
+	h = game.Players./localplayer/.Character.Head.Mesh
 	t = Instance.new("SpecialMesh",game.Workspace[plr].Torso)
 	la = Instance.new("SpecialMesh",plr["Left Arm"])
 	ra = Instance.new("SpecialMesh",plr["Right Arm"])
